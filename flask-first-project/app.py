b2429bb7fa574b45380f7de92b3b6713028fd5d8
Syr7-s <isasayar7@github.com>
2021-09-06 23:09:32 +0300

    app.py file was edited. Also flask-first-project.md file was created and filled.

--- flask-first-project/app.py
+++ flask-first-project/app.py
@@ -2,11 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+host = "localhost"
+port = 8080
+
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/login/<string:name>/<int:age>/')
+def login(name, age):
+    return 'hello ' + name + ', is ' + str(age) + ' years old ? Really'
+
+
 if __name__ == '__main__':
     app.run()
+# use_reloader = True
