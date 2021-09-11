8556885a7024765a740bfe8bbe2d25951b24c48f
Syr7-s <isasayar7@github.com>
2021-09-11 20:49:33 +0300

    app.py was edited.

--- flask-url-project/app.py
+++ flask-url-project/app.py
@@ -13,5 +13,9 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/your-url')
+def your_url():
+    return render_template('your_url.html')
+
 if __name__ == '__main__':
     app.run()
