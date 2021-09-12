87dbf9bc383c9d79f20698a74044e62c6a1deccf
Syr7-s <isasayar7@github.com>
2021-09-12 16:05:32 +0300

    app.py and flask-info.md was edited.

--- flask-url-project/app.py
+++ flask-url-project/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+import json
 
 app = Flask(__name__)
 
@@ -16,9 +17,13 @@ def home():
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
+        urls = {} #empty dictionary
+        urls[request.form['code']] = {'url': request.form['url']}
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
         return render_template('your_url.html', code=request.form['code'])
     else:
-        return jsonify(message='This is not valid ')
+        return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
